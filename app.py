32004780038467d977292ef017a06c51207ea14d
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-28 16:56:59 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# escape sequences in python
+# /"
+# /'
+# //
+# /n
+
+print("hello world")
+x = 1
+name = "ghislain"
+# print(len(name))
+# print(name[0])
+# print(name[-1])
+# print(name[0:3])
+
+first = "byimbo"
+second = "ghislain"
+
+# full_name = first + " " + second
+full_name = f"{first} {second}"
+print(full_name)
+
+
+# printing type of variables
+
+print(type("hello"))
+print(type(2))
+print(type(True))
+print(type(1.43))
+
+# type casting
+
+a = str(10)
+b = str("abc")
+c = str(3.5)
+d = int("3")
+e = int(3.5)
+f = int(3.5)
+
+
+
+
