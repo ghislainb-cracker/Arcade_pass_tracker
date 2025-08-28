4bf4f17a9b38da40c39d31546368a9d481a13026
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-28 17:02:58 +0200

    sixth commit

--- app.py
+++ app.py
@@ -39,4 +39,32 @@ g = float(1)
 h = float("32")
 i = float("12.4")
 
-print([a, b, c, d, e, f, g, h, i])
+print([a, b, c, d, e, f, g, h, i]);
+
+# 🕹️ Arcade Day Pass Tracker — Challenge Steps
+#
+# 1) Create variables to store:
+#    - customer name
+#    - number of passes
+#    - tokens per pass
+#    - price per pass
+#    - tokens required per game
+
+customer_name = "ghislain"
+number_of_passes = 3
+token_per_pass = 30
+price_per_pass =15.00
+tokens_per_game = 3
+#
+# 2) Calculate:
+#    - total tokens
+#    - total cost
+#    - games available  (use 'floor division' to get a whole number)
+total_tokens = number_of_passes * token_per_pass
+#
+# 3) Print a summary with:
+#    - customer name
+#    - passes bought
+#    - total tokens
+#    - total cost
+#    - games available
